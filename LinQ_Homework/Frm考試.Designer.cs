0757bdc8920dab2b3f1d07c3b19a887c060a8034
rovingwind93@gmail.com <rovingwind93@gmail.com>
2021-01-25 11:45:20 +0800

    123

--- LinQ_Homework/Frm考試.Designer.cs
+++ LinQ_Homework/Frm考試.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.button35 = new System.Windows.Forms.Button();
             this.button34 = new System.Windows.Forms.Button();
@@ -42,9 +42,9 @@
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.lblDetails = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -57,9 +57,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -93,6 +93,7 @@
             this.button35.TabIndex = 143;
             this.button35.Text = "所有隨機分數出現的次數/比率";
             this.button35.UseVisualStyleBackColor = false;
+            this.button35.Click += new System.EventHandler(this.button35_Click);
             // 
             // button34
             // 
@@ -221,6 +222,23 @@
             this.panel1.Size = new System.Drawing.Size(526, 246);
             this.panel1.TabIndex = 103;
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(526, 246);
+            this.chart1.TabIndex = 105;
+            this.chart1.Text = "chart1";
+            // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -244,23 +262,6 @@
             this.pictureBox1.TabIndex = 103;
             this.pictureBox1.TabStop = false;
             // 
-            // chart1
-            // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(0, 0);
-            this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(526, 246);
-            this.chart1.TabIndex = 105;
-            this.chart1.Text = "chart1";
-            // 
             // Frm考試
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -281,9 +282,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
--- LinQ_Homework/Frm考試.cs
+++ LinQ_Homework/Frm考試.cs
@@ -39,7 +39,7 @@ namespace LinQ_Homework
 
         private void button6_Click(object sender, EventArgs e)
         {
-            var q = from od in nwEntities.Order_Details
+            var q = (from od in nwEntities.Order_Details
                     group (float)(od.UnitPrice * od.Quantity) * (1.0 - od.Discount)
                     by od.Order.OrderDate.Value.Year into g
                     orderby g.Key
@@ -47,22 +47,45 @@ namespace LinQ_Homework
                     {
                         g.Key,
                         Sum = g.Sum()
-                    };
-            var list = q.ToList();
-            var q2 = from i in Enumerable.Range(0, list.Count)
+                    }).ToList();
+            
+            var q2 = (from i in Enumerable.Range(0, q.Count)
                      select new
                      {
-                         Year = list[i].Key,
-                         營收 = list[i].Sum,
-                         成長率 = (i - 1 >= 0) ? ((list[i].Sum - list[i - 1].Sum) / list[i - 1].Sum)*100 : 0
-                     };
-
-            var list2 = q2.ToList();
-            dataGridView1.DataSource = list2;
-            chart1.DataSource = list2;
+                         Year = q[i].Key,
+                         營收 = q[i].Sum,
+                         成長率 = (i - 1 >= 0) ? ((q[i].Sum - q[i - 1].Sum) / q[i - 1].Sum)*100 : 0
+                     }).ToList();
+                        
+            dataGridView1.DataSource = q2;
+            chart1.DataSource = q2;
             chart1.Series[0].XValueMember = "Year";
             chart1.Series[0].YValueMembers = "成長率";
             chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
         }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int[] nums = new int[33];
+            for (int i = 0; i <= nums.Length - 1; i++)
+                nums[i] = r.Next(1, 11);
+            var q = from n in nums.AsEnumerable()
+                    group n by n into g
+                    orderby g.Key ascending
+                    select new
+                    {
+                        Mykey = g.Key,
+                        次數 = g.Count(),
+                        出現率百分比 = $"{((float)g.Count() / (float)nums.Length *100):f2}"
+                    };
+            this.dataGridView1.DataSource = q.ToList();
+
+            this.chart1.DataSource = q.ToList();
+            this.chart1.Series[0].XValueMember = "MyKey";
+            this.chart1.Series[0].YValueMembers = "出現率百分比";
+            this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            this.chart1.Series[0].Name = "數字出現率";
+        }        
     }
 }
